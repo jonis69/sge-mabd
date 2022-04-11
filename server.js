11fb13da9fd0350f86ccdbc806103e601f88015e
Jonas <jonas@HACKING-MACHINE>
2022-04-11 10:48:11 -0300

    Configuração inicial do sistema

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+    res.json({ message: "seja wellcomido" });
+});
+
+const allRoutes = require('./app/routes/routes');
+app.use('/api', allRoutes);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}.`);
+});
+
+const db = require("./app/models");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Conectado ao banco de dados");
+    })
+    .catch(err => {
+        console.log("Não foi possível conectar-se ao banco de dados", err);
+        process.exit();
+    });
