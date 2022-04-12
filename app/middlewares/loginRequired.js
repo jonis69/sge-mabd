b125d183258ee0157b92f170b08d6fde1c77b2b4
Jonas <jonas@HACKING-MACHINE>
2022-04-12 15:09:47 -0300

    Criado um middleware de autenticação

---
+++ app/middlewares/loginRequired.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { Unauthorized } = require('../config/errors');
+
+module.exports = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    throw new Unauthorized();
+  }
+
+  const [, token] = authorization.split(' '); // Vai vir: "Bearer(espaço)Token".  por isso o split no token
+
+  try {
+    const data = jwt.verify(token, process.env.SECRET_TOKEN); // Dados que estão no payload do token
+    const { id, email } = data; // Extraindo id e email que vieram no payload
+
+    // Verificando se é o mesmo usuário (caso tenha atualizado o email, o token não será mais válido)
+    const user = await User.findOne({
+      where: {
+        id,
+        email,
+      },
+    });
+
+    if (!user) { // Se o usuário atualizou seu EMAIL, ele terá que realizar login novamente
+      throw new Unauthorized('Invalid user, login again');
+    }
+
+    // id e email do usuário agora estão disponíveis e são conhecidos no sistema
+    req.userId = id;
+    req.userEmail = email;
+    return next(); // Continua para a response
+
+  } catch (err) {
+    if(err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
+      throw new Unauthorized('Expired or invalid token');
+    }
+    next(err);
+  }
+};
