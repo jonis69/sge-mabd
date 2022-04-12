ab71a1c8f575bc467e8a62ed74d3856c25018971
Jonas <jonas@HACKING-MACHINE>
2022-04-12 15:06:30 -0300

    Adicionadas as rotas de usuário

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -11,5 +11,8 @@ router.use('/agreements', agreementRouter);
 // ---------- Estágios ----------
 const internshipRouter = require('./internship');
 router.use('/internships', internshipRouter);
+// ---------- Login e registro de usuário ----------
+const userRouter = require('./user');
+router.use('/users', userRouter);
 
 module.exports = router;
