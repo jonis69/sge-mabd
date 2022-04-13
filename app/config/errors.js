9b3815c3af3ee68ad25f660e65b3a6524382ba98
Jonas <jonas@HACKING-MACHINE>
2022-04-13 11:16:57 -0300

    Erros que podem ser usados na aplicação

--- app/config/errors.js
+++ app/config/errors.js
@@ -1,50 +1,50 @@
 // Aqui são definidos os erros que podemos utilizar na aplicação
 
-class GeneralError extends Error { // Obtem a mensagem e o código de status
+class GeneralError extends Error {
+    // Obtem a mensagem e o código de status
     constructor(message, name, code = 500) {
-      super();
-      this.message = message;
-      this.name = name;
-      this.code = code;
+        super();
+        this.message = message;
+        this.name = name;
+        this.code = code;
     }
-  }
-  
-  class BadRequest extends GeneralError {
+}
+
+class BadRequest extends GeneralError {
     constructor(message, name) {
-      super(message, name, 400);
-      this.message = message || 'Invalid requisition';
-      this.name = 'BadRequest';
+        super(message, name, 400);
+        this.message = message || "Invalid requisition";
+        this.name = "BadRequest";
     }
-  }
-  class NotFound extends GeneralError {
+}
+class NotFound extends GeneralError {
     constructor(message, name) {
-      super(message, name, 404);
-      this.message = message || 'Resource not found';
-      this.name = 'NotFound';
+        super(message, name, 404);
+        this.message = message || "Resource not found";
+        this.name = "NotFound";
     }
-  }
-  
-  class Unauthorized extends GeneralError {
+}
+
+class Unauthorized extends GeneralError {
     constructor(message, name) {
-      super(message, name, 401);
-      this.message = message || 'Access is allowed only for registered users';
-      this.name = 'Unauthorized'
+        super(message, name, 401);
+        this.message = message || "Access is allowed only for registered users";
+        this.name = "Unauthorized";
     }
-  }
-  
-  // class Conflict extends GeneralError {
-  //   constructor(message, name) {
-  //     super(message, name, 409);
-  //     this.message = message || 'Resource already exists';
-  //     this.name = 'Conflict';
-  //   }
-  // }
-  
-  module.exports = {
+}
+
+// class Conflict extends GeneralError {
+//   constructor(message, name) {
+//     super(message, name, 409);
+//     this.message = message || 'Resource already exists';
+//     this.name = 'Conflict';
+//   }
+// }
+
+module.exports = {
     GeneralError,
     BadRequest,
     NotFound,
     Unauthorized,
     // Conflict
-  };
-  
+};
